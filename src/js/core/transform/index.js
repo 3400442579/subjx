f1967751fd0b50c8f12a888438f858b66c416dde
jianxingwu <jianxingwu@dj.cn>
2020-11-23 17:33:18 +0800

    移除 clone,svg 操作

--- src/js/core/transform/index.js
+++ src/js/core/transform/index.js
@@ -1,7 +1,7 @@
 import { Observable } from '../observable';
 import Draggable from './Draggable';
-import DraggableSVG from './svg';
-import { checkElement } from './svg/util';
+//import DraggableSVG from './svg';
+//import { checkElement } from './svg/util';
 import { arrReduce, isDef } from '../util/util';
 
 // factory method for creating draggable elements
@@ -12,17 +12,17 @@ export default function drag(options, obInstance) {
             : new Observable();
 
         return arrReduce.call(this, (result, item) => {
-            if (!(item instanceof SVGElement)) {
-                result.push(
-                    new Draggable(item, options, Ob)
-                );
-            } else {
-                if (checkElement(item)) {
-                    result.push(
-                        new DraggableSVG(item, options, Ob)
-                    );
-                }
-            }
+            //if (!(item instanceof SVGElement)) {
+            result.push(
+                new Draggable(item, options, Ob)
+            );
+            //} else {
+            //    if (checkElement(item)) {
+            //        result.push(
+            //            new DraggableSVG(item, options, Ob)
+            //        );
+            //    }
+            //}
             return result;
         }, []);
     }
